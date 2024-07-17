6853f49fb849bc668f0a8d472178c3a3da2b5d10
ausra <ausra.balyne@gmail.com>
2024-07-17 16:10:24 +0300

    updated json decoder version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
-        .package(url: "https://github.com/Ausra/JSONLDDecoder.git", from: "1.0.7")
+        .package(url: "https://github.com/Ausra/JSONLDDecoder.git", from: "1.0.8")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
