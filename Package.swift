669d318cf35cd816f5bd14a29f5a1f6b05918f6b
ausra <ausra.balyne@gmail.com>
2024-07-17 15:55:12 +0300

    updated tools version to be compatable with xcode 15

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
-        .package(url: "https://github.com/Ausra/JSONLDDecoder.git", from: "1.0.6")
+        .package(url: "https://github.com/Ausra/JSONLDDecoder.git", from: "1.0.7")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
